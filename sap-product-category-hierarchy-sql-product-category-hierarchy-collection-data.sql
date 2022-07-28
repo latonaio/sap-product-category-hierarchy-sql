54f498c70a2495e922fdc1f71db9c6cae1a677d2
Yurino Oga <yurino.o@latona.io>
2022-07-28 15:01:12 +0900

    release

---
+++ sap-product-category-hierarchy-sql-product-category-hierarchy-collection-data.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `sap_perfect_store_execution_store_valuation_collection_data`
+(
+		`ObjectID`                         varchar(70)    NOT NULL,
+		`ETag`                             varchar(130)   DEFAULT NULL,
+		`RootProductCategoryInternalID`    varchar(20) 　　DEFAULT NULL,
+		`ProductCategoryInternalID`        varchar(20) 　　DEFAULT NULL,
+		`ParentProductCategoryInternalID`  varchar(20) 　　DEFAULT NULL,
+		`LanguageCode`                     varchar(20) 　　DEFAULT NULL,
+		`LanguageCodeText`                 varchar(80) 　　DEFAULT NULL,
+		`ProductCategory`                  varchar(80) 　　DEFAULT NULL,
+		`ProductAssignmentAllowed`         tinyint(1)  　　DEFAULT NULL,
+		PRIMARY KEY (`ObjectID`) 
+	 ) ENGINE = InnoDB
+	 DEFAULT CHARSET = utf8mb4;
